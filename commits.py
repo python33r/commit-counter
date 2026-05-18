8d09546973a091c2d237f472e1b26f2c469b6569
Nick Efford <nick.efford@gmail.com>
2026-05-18 22:39:53 +0100

    Use better method to calculate weekly totals

--- commits.py
+++ commits.py
@@ -12,15 +12,16 @@
 
 import os
 import sys
-import datetime as dt
 from collections import Counter
-from datetime import date
+from datetime import date, timedelta
 from subprocess import run
 
 
+type Details = tuple(list[str], list[date])
+
 DATE_FORMAT = "%a %b %d %H:%M:%S %Y %z"
 
-type Details = tuple(list[str], list[date])
+SEVEN_DAYS = timedelta(days=7)
 
 
 def get_commit_details() -> Details:
@@ -31,14 +32,14 @@ def get_commit_details() -> Details:
     lines = result.stdout.decode().split("\n")
     names = [line[8:].strip() for line in lines if line.startswith("Author: ")]
     dates = [
-        dt.date.strptime(line[6:].strip(), DATE_FORMAT)
+        date.strptime(line[6:].strip(), DATE_FORMAT)
         for line in lines
         if line.startswith("Date: ")
     ]
     return names, dates
 
 
-def display_individual_totals(names: list[str]):
+def display_individual_counts(names: list[str]):
     """
     Display individual commit counts, ranked highest to lowest,
     and a total count across all committers.
@@ -50,25 +51,27 @@ def display_individual_totals(names: list[str]):
     print(f"{counts.total():3}")
 
 
+def shift_to_monday(d: date) -> date:
+    year, week, _ = d.isocalendar()
+    return date.fromisocalendar(year, week, 1)
+
+
 def display_weekly_totals(dates: list[date]):
     """
     Display total commits per week for the time period spanned by commits.
-
-    Note: it is assumed that the time period is within a single year.
     """
-    dates.sort()
-    year = dates[0].year
-    week_counts = Counter(d.isocalendar()[1] for d in dates)
-    first_week = min(week_counts.keys())
-    last_week = max(week_counts.keys())
+    totals = Counter(shift_to_monday(d) for d in dates)
 
     print("\n+------------+---------+")
     print("| Week       | Commits |")
     print("+------------+---------+")
-    for week in range(first_week, last_week + 1):
-        start_of_week = date.fromisocalendar(year, week, 1)
-        count = week_counts.get(week, 0)
-        print(f"| {start_of_week} |   {count:3}   |")
+
+    week_start = shift_to_monday(min(dates))
+    while week_start <= shift_to_monday(max(dates)):
+        weekly_total = totals.get(week_start, 0)
+        print(f"| {week_start} |   {weekly_total:3}   |")
+        week_start += SEVEN_DAYS
+
     print("+----------------------+")
 
 
@@ -77,5 +80,5 @@ if __name__ == "__main__":
         os.chdir(sys.argv[1])
 
     names, dates = get_commit_details()
-    display_individual_totals(names)
+    display_individual_counts(names)
     display_weekly_totals(dates)
