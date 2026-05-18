825be10bc9465cfcc9f051d1979e35a141d867de
Nick Efford <nick.efford@gmail.com>
2026-05-18 12:46:27 +0100

    Added feature to count weekly totals

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # commit-counter
 
-Simple program to count individual commits in a team's Git repository.
+A program to count commits in a team's Git repository.
+
+Individual totals are displayed for each committer, followed by the per-week
+totals across the whole team, for the time period spanned by the commits.
 
 Run this in the usual way, with the path to the cloned repository as a
 command line argument. You can also run it with no command line arguments.
--- commits.py
+++ commits.py
@@ -1,9 +1,9 @@
 # /// script
-# requires-python = ">=3.12"
+# requires-python = ">=3.14"
 # dependencies = []
 # ///
 
-# Program to gather stats on individual team member commits.
+# Program to gather stats on team member commits & per-week totals.
 # (Useful when GitHub is being flaky!)
 
 # You can supply the path to a cloned repository on the command line.
@@ -12,34 +12,70 @@
 
 import os
 import sys
+import datetime as dt
 from collections import Counter
+from datetime import date
 from subprocess import run
 
 
-def get_committer_names() -> list[str]:
+DATE_FORMAT = "%a %b %d %H:%M:%S %Y %z"
+
+type Details = tuple(list[str], list[date])
+
+
+def get_commit_details() -> Details:
     """
     Extract names from 'Author:' lines in git log output.
     """
     result = run(["git", "log"], capture_output=True)
     lines = result.stdout.decode().split("\n")
-    return [line[8:] for line in lines if line.startswith("Author: ")]
+    names = [line[8:].strip() for line in lines if line.startswith("Author: ")]
+    dates = [
+        dt.date.strptime(line[6:].strip(), DATE_FORMAT)
+        for line in lines
+        if line.startswith("Date: ")
+    ]
+    return names, dates
 
 
-def display_commit_counts(counts: Counter):
+def display_individual_totals(names: list[str]):
     """
-    Display commit counts, ranked highest to lowest, and a total.
+    Display individual commit counts, ranked highest to lowest,
+    and a total count across all committers.
     """
-    items = counts.items()
-    for name, count in sorted(items, key=lambda x: x[1], reverse=True):
-        print(f"{count:3} {name}")
+    counts = Counter(names)
+    for item in sorted(counts.items(), key=lambda x: x[1], reverse=True):
+        print(f"{item[1]:3} {item[0]}")
     print("---")
     print(f"{counts.total():3}")
+
+
+def display_weekly_totals(dates: list[date]):
+    """
+    Display total commits per week for the time period spanned by commits.
+
+    Note: it is assumed that the time period is within a single year.
+    """
+    dates.sort()
+    year = dates[0].year
+    week_counts = Counter(d.isocalendar()[1] for d in dates)
+    first_week = min(week_counts.keys())
+    last_week = max(week_counts.keys())
+
+    print("\n+------------+---------+")
+    print("| Week       | Commits |")
+    print("+------------+---------+")
+    for week in range(first_week, last_week + 1):
+        start_of_week = date.fromisocalendar(year, week, 1)
+        count = week_counts.get(week, 0)
+        print(f"| {start_of_week} |   {count:3}   |")
+    print("+----------------------+")
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         os.chdir(sys.argv[1])
 
-    names = get_committer_names()
-    commits = Counter(names)
-    display_commit_counts(commits)
+    names, dates = get_commit_details()
+    display_individual_totals(names)
+    display_weekly_totals(dates)
